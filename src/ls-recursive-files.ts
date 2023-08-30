caa2fda430458d8f16947dc287756efe35e2dc85
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2023-08-29 20:37:40 -0700

    feat: different scripts for dirs, files, and paths
    note: I should probably have just used yargs

---
+++ src/ls-recursive-files.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env ts-node-script
+import fs from 'fs';
+import path from 'path';
+
+const args = process.argv;
+
+const targetDir = args[2];
+const dir = path.join(targetDir, args[3] || '.');
+
+function listRelativePaths(directoryPath: string, currentPath = ''): void {
+  const items = fs.readdirSync(directoryPath);
+
+  items.forEach(item => {
+    const itemPath = path.join(directoryPath, item);
+    const relativePath = path.join(currentPath, item);
+
+    if (fs.statSync(itemPath).isFile()) {
+      console.log(relativePath);
+    } else {
+    }
+
+    if (fs.statSync(itemPath).isDirectory()) {
+      listRelativePaths(itemPath, relativePath);
+    }
+  });
+}
+
+listRelativePaths(dir);
